240f763b924dfc529630a8d801dd9a606c787577
Gabriel Palominos Teiga <116650677+gabrielteiga@users.noreply.github.com>
2022-12-20 13:56:19 -0300

    Implementado a interface para seleção do tipo de requisição. Audio, Video highQuality ou Video lowDataConsumption

--- baixar_youtube_musica.py
+++ baixar_youtube_musica.py
@@ -11,7 +11,8 @@ def extract_audio(url):
     return url.streams.filter(only_audio=True).first()
 
 
-def download_audio(url):
+def download_audio():
+    url = ask_to_user()
     audio = extract_audio(url)
 
     out_file = audio.download('musicas_python')
@@ -23,10 +24,32 @@ def download_audio(url):
     print(url.title + ' audio has been sucessfully downloaded')
 
 
+def download_high_quality_video():
+    print('Feature in progress...')
+
+
+def download_low_quality_video():
+    print('Feature in progress...')
+
+
 if __name__ == '__main__':
     try:
-        url = ask_to_user()
-        download_audio(url)
+        request = int(input(
+            """Choose which one to download:
+    1 - .mp3
+    2 - .mp4 high quality
+    3 - .mp4 low data consumption
+Response: """
+        ))
+
+        if request == 1:
+          download_audio()
+        elif request == 2:
+          download_high_quality_video()
+        elif request == 3:
+          download_low_quality_video()
+        else:
+          raise ValueError('Invalid request!!!')
 
     except:
         raise ValueError("Input a valid URL... If the issue persist, try another link.")
