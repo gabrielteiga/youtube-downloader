87cdbd24bb68877afced2f801be699949b837e74
Gabriel Palominos Teiga <116650677+gabrielteiga@users.noreply.github.com>
2022-12-20 02:54:10 -0300

    Criação de módulos para melhorar a leitura do código.

--- baixar_youtube_musica.py
+++ baixar_youtube_musica.py
@@ -1,29 +1,32 @@
-# Devemos executar o programa no cmd: python nome_arquivo.py "www.youtube.com/link_para_analise"
-# A biblioteca "sys" vai fazer com que ele seja executado
-# from sys import argv
 from pytube import YouTube
 import os
 
-try:
 
-    # link = argv[1] Com o sys, pegamos direto do prompt de comando
-    # URL do Youtube
+def ask_to_user():
     link = str(input("Digite a URL do vídeo que será extraído o áudio: "))
-    yt = YouTube(link)
+    return YouTube(link)
 
-    # Extraindo apenas o audio
-    yd = yt.streams.filter(only_audio=True).first()  # Dessa maneira o arquivo fica mais leve
 
-    # Download do arquivo
-    out_file = yd.download('musicas_python')
+def extract_audio(url):
+    return url.streams.filter(only_audio=True).first()
 
-    # Salvando como .mp3
+
+def download_audio(url):
+    audio = extract_audio(url)
+
+    out_file = audio.download('musicas_python')
+
     base, ext = os.path.splitext(out_file)
-    new_file = base + ".mp3"
+    new_file = base + '.mp3'
     os.rename(out_file, new_file)
 
-    # Fim
-    print(yt.title + " has been sucessfully downloaded")
+    print(url.title + ' audio has been sucessfully downloaded')
 
-except:
-    raise ValueError("Deu um problema no download, tente novamente com uma url valida...")
+
+if __name__ == '__main__':
+    try:
+        url = ask_to_user()
+        download_audio(url)
+
+    except:
+        raise ValueError("Input a valid URL... If the issue persist, try another link.")
